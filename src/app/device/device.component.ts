ce08f26bc6bec938d0827cc7cdc3603933147a92
Baptiste  Geffroy <baptiste.geffroy@ekino.com>
2021-06-07 16:26:05 +0200

    modify the declaration of deviceStatus with the @Input() decorator

--- src/app/device/device.component.ts
+++ src/app/device/device.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DeviceComponent implements OnInit {
   @Input() deviceName!: string;
-  deviceStatus: string = 'éteint';
+  @Input() deviceStatus!: string;
 
   constructor() {}
 
