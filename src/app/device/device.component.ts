83664159c7794ad61e23368083cae3dd66927761
Baptiste  Geffroy <baptiste.geffroy@ekino.com>
2021-06-07 15:21:09 +0200

    create 2 variable for test string interpolation

--- src/app/device/device.component.ts
+++ src/app/device/device.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-device',
   templateUrl: './device.component.html',
-  styleUrls: ['./device.component.scss']
+  styleUrls: ['./device.component.scss'],
 })
 export class DeviceComponent implements OnInit {
+  deviceName: string = 'Machine à laver';
+  deviceStatus: string = 'éteint';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
