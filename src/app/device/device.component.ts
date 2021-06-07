28fafbfb0c27ed7fbc7db0e90c8a51db1b44443d
Baptiste  Geffroy <baptiste.geffroy@ekino.com>
2021-06-07 16:11:17 +0200

    modify the declaration of deviceName with the @Input() decorator

--- src/app/device/device.component.ts
+++ src/app/device/device.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-device',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./device.component.scss'],
 })
 export class DeviceComponent implements OnInit {
-  deviceName: string = 'Machine à laver';
+  @Input() deviceName!: string;
   deviceStatus: string = 'éteint';
 
   constructor() {}
