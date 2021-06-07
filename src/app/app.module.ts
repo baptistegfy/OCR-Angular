f697ef6a94bda728bbf3c52cc94f66c8777ee2c7
Baptiste  Geffroy <baptiste.geffroy@ekino.com>
2021-06-07 16:03:54 +0200

    import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +8,9 @@ import { MyFirstComponent } from './my-first/my-first.component';
 import { DeviceComponent } from './device/device.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MyFirstComponent,
-    DeviceComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MyFirstComponent, DeviceComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
