280b7c2391634ac5ace124698f8057ba354c2344
Baptiste  Geffroy <baptiste.geffroy@ekino.com>
2021-06-08 23:37:48 +0200

    linked function signin signout

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
+  authStatus!: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.authStatus = this.authService.isAuth;
   }
 
+  onSignIn() {
+    this.authService.signIn().then(() => {
+      console.log('Connexion réussie !');
+      this.authStatus = this.authService.isAuth;
+    });
+  }
+
+  onSignOut() {
+    this.authService.signOut();
+    this.authStatus = this.authService.isAuth;
+  }
 }
