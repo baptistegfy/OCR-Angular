a7892a005b32ba88ae63512528d17989a5290a7a
Baptiste  Geffroy <baptiste.geffroy@ekino.com>
2021-06-09 01:56:24 +0200

    modify template and update Ts for use the params url (id)

--- src/app/single-device/single-device.component.ts
+++ src/app/single-device/single-device.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DeviceService } from '../services/device.service';
 
 @Component({
   selector: 'app-single-device',
   templateUrl: './single-device.component.html',
-  styleUrls: ['./single-device.component.scss']
+  styleUrls: ['./single-device.component.scss'],
 })
 export class SingleDeviceComponent implements OnInit {
+  name: string = 'Appareil';
+  status: string = 'Status';
 
-  constructor() { }
+  constructor(
+    private deviceService: DeviceService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    this.name = this.deviceService.getDeviceById(+id).name;
+    this.status = this.deviceService.getDeviceById(+id).status;
   }
-
 }
