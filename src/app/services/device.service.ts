9f59cd95560cbc7dfa5c82bcb0cb4a991428c32b
Baptiste  Geffroy <baptiste.geffroy@ekino.com>
2021-06-09 01:52:09 +0200

    add id on each obj od the array of device ans create the function getDeviceById

--- src/app/services/device.service.ts
+++ src/app/services/device.service.ts
@@ -1,30 +1,45 @@
+import { Device } from '../types/device';
+
 export class DeviceService {
-  devices = [
+  devices: Device[] = [
     {
+      id: 1,
       name: 'Machine à laver',
       status: 'éteint',
     },
     {
+      id: 2,
       name: 'Télévision',
       status: 'allumé',
     },
     {
+      id: 3,
       name: 'Ordinateur',
       status: 'allumé',
     },
     {
+      id: 4,
       name: 'Téléphone',
       status: 'allumé',
     },
     {
+      id: 5,
       name: 'Enceinte',
       status: 'allumé',
     },
     {
+      id: 6,
       name: 'Vidéo projecteur',
       status: 'éteint',
     },
   ];
+
+  getDeviceById(id: number): any {
+    const device = this.devices.find((obj) => {
+      return obj.id === id;
+    });
+    return device;
+  }
 
   switchOnAll() {
     this.devices.forEach((device) => {
