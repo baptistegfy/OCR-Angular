d3e0f820e23cf9b37a061f450bf335678511fe4b
Baptiste  Geffroy <baptiste.geffroy@ekino.com>
2021-06-08 22:18:57 +0200

    modification because 2 component as create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { AppComponent } from './app.component';
 import { MyFirstComponent } from './my-first/my-first.component';
 import { DeviceComponent } from './device/device.component';
 import { DeviceService } from './services/device.service';
+import { AuthComponent } from './auth/auth.component';
+import { DeviceViewComponent } from './device-view/device-view.component';
 
 @NgModule({
-  declarations: [AppComponent, MyFirstComponent, DeviceComponent],
+  declarations: [AppComponent, MyFirstComponent, DeviceComponent, AuthComponent, DeviceViewComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [DeviceService],
   bootstrap: [AppComponent],
