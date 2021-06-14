873b745f8cd5f0c158fbde23ac7759e120a95fb1
Baptiste  Geffroy <baptiste.geffroy@ekino.com>
2021-06-14 12:03:44 +0200

    change syntax of constructor new User (formValue['fistname'] ......  to formValue.firstname ... and insert 2 validator of email in an array

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -25,7 +25,7 @@ export class NewUserComponent implements OnInit {
     this.useForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      email: ['', Validators.required, Validators.email],
+      email: ['', [Validators.required, Validators.email]],
       drinkPreference: ['', Validators.required],
       hobbies: this.formBuilder.array([]),
     });
@@ -34,11 +34,11 @@ export class NewUserComponent implements OnInit {
   onSubmitForm() {
     const formValue = this.useForm.value;
     const newUser = new User(
-      formValue['firstName'],
-      formValue['lastName'],
-      formValue['email'],
-      formValue['drinkPreference'],
-      formValue['hobbies'] ? formValue['hobbies'] : []
+      formValue.firstName,
+      formValue.lastName,
+      formValue.email,
+      formValue.drinkPreference,
+      formValue.hobbies ? formValue.hobbies : []
     );
     this.userService.addUser(newUser);
     this.router.navigate(['/users']);
